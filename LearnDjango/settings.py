941ec623fb681d5752bcf5eb653241243fd16623
liupeng <liupeng.0@outlook.com>
2020-06-26 17:53:29 +0800

    切换数据库 V 2.0.8

--- LearnDjango/settings.py
+++ LearnDjango/settings.py
@@ -78,14 +78,14 @@ WSGI_APPLICATION = 'LearnDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'ENGINE': 'django.db.backends.mysql',   # 数据库引擎
-        # 'NAME': 'mysite',  # 数据库名，先前创建的
-        # 'USER': 'root',     # 用户名，可以自己创建用户
-        # 'PASSWORD': 'Lp184126',  # 密码
-        # 'HOST': '114.116.4.196',  # mysql服务所在的主机ip
-        # 'PORT': '3306',         # mysql服务端口
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',   # 数据库引擎
+        'NAME': 'learnDjango',  # 数据库名，先前创建的
+        'USER': 'root',     # 用户名，可以自己创建用户
+        'PASSWORD': 'Lp184126',  # 密码
+        'HOST': '114.116.4.196',  # mysql服务所在的主机ip
+        'PORT': '3306',         # mysql服务端口
     }
 }
 
